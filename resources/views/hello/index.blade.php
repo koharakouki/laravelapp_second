7022f548a3e4d5ed4839d55fb775c8f62dfae9f5
koharakouki <ko.kohara02@gmail.com>
2020-10-13 22:27:36 +0900

    [add] 継承レイアウト

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,4 +1,4 @@
-<html>
+<!-- <html>
 <head>
 	<title>Hello/Index</title>
 	<style>
@@ -25,3 +25,23 @@
 </body>
 </html>
 
+ -->
+
+
+@extends('layouts.helloapp')
+
+@section('title', 'Index')
+
+@section('menubar')
+  @parent
+  インデックスページ
+@endsection
+
+@section('content')
+  <p>ここが本文のコンテンツです</p>
+  <p>必要なだけ記述ができます</p>
+@endsection
+
+@section('footer')
+  copyright 2017 unko.
+@endsection
